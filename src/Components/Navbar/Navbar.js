9053d99cd596e1c23bb571e87bf2393ff6627335
rosekamallove <rosekamallove@gmail.com>
2021-07-29 22:48:20 +0530

    change navbar location

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <img src="/" alt="logo" className="nav-logo" />
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
