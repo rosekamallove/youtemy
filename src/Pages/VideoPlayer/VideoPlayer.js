4b44dc7adf8f2e3f9c1ea7f2835bbbedf6a2c7a6
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-03 22:01:31 +0530

    menu-item onclick

--- src/Pages/VideoPlayer/VideoPlayer.js
+++ src/Pages/VideoPlayer/VideoPlayer.js
@@ -1,8 +1,7 @@
-import { Layout, Menu, Typography } from "antd";
+import { Checkbox, Layout, Menu, Typography } from "antd";
 import "antd/dist/antd.css";
-import Checkbox from "antd/lib/checkbox/Checkbox";
 import React, { useEffect, useState } from "react";
-import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/youtube";
 import "./VideoPlayer.css";
 
 const { Header, Sider, Content } = Layout;
@@ -12,9 +11,10 @@ const VideoPlayer = ({ playlistID, userProgress }) => {
   const [state, setState] = useState({
     menuCollapsed: 0,
     firstVideo: "",
-    currentVideo: "",
     playlistArray: [],
   });
+
+  const [currentVideo, setCurrentVideo] = useState("");
 
   useEffect(() => {
     const API_KEY = "AIzaSyBR3F9lodP7zQ3wiY3FY0dHS_8edP5j6NM";
@@ -27,7 +27,7 @@ const VideoPlayer = ({ playlistID, userProgress }) => {
     const options = {
       part: "snippet",
       key: API_KEY,
-      maxResults: 20,
+      maxResults: 100,
       playlistId: playlistID,
     };
 
@@ -45,22 +45,33 @@ const VideoPlayer = ({ playlistID, userProgress }) => {
           firstVideo: data.items[0].snippet.resourceId.videoId,
           playlistArray: data.items,
         });
+        setCurrentVideo(state.firstVideo);
       })
       .catch((err) => console.log(err));
   }, []);
-
   /* Utility */
-  const returnIframMarkup = (videoId) => {
-    let videoURL = `https://www.youtube.com/embed/${videoId}`;
+  const returnIframMarkup = () => {
+    let videoURL = `https://www.youtube.com/embed/${
+      currentVideo || state.firstVideo
+    }`;
     return (
       <ReactPlayer
+        controls={true}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
         url={videoURL}
         className="react-player"
-        width="100%"
-        height="80%"
+        width="auto%"
+        height={700}
+        onEnded={handleVideoEnded}
+        onDuration={handleVideoDuration}
       ></ReactPlayer>
     );
   };
+
+  const handleVideoEnded = () => {};
+  const handleVideoDuration = () => {};
+
   const handleMenuCollapse = (collapsed) => {
     setState({
       ...state,
@@ -69,54 +80,59 @@ const VideoPlayer = ({ playlistID, userProgress }) => {
   };
 
   const handleMenuItemClick = (videoId) => {
-    returnIframMarkup(videoId);
+    setCurrentVideo(videoId);
   };
 
   return (
-    <div>
-      <Layout>
-        <Sider
-          collapsible
-          onCollapse={handleMenuCollapse}
-          collapsed={state.menuCollapsed}
-          width={400}
-          collapsedWidth={150}
+    <Layout>
+      <Sider
+        collapsible
+        onCollapse={handleMenuCollapse}
+        collapsed={state.menuCollapsed}
+        width={400}
+        collapsedWidth={150}
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="light"
+          mode="inline"
+          defaultSelectedKeys={JSON.stringify(currentVideo)}
+          className="menu"
         >
-          <Menu
-            theme="light"
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            className="menu"
-          >
-            <Title level={4} className="playlist-title">
-              Videos
-            </Title>
-            {state.playlistArray.map((item) => (
-              <Menu.Item
-                key={item.snippet.resourceId.videoId}
-                className="menu-item"
-                onClick={handleMenuItemClick(item.snippet.resourceId.videoId)}
-              >
-                <Checkbox className="menu-checkbox"></Checkbox>
-                {item.snippet.title}
-              </Menu.Item>
-            ))}
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Content
+          <Title level={4} className="playlist-title">
+            Videos
+          </Title>
+          {state.playlistArray.map((item) => (
+            <Menu.Item
+              key={item.snippet.resourceId.videoId}
+              className="menu-item"
+              onClick={() => {
+                handleMenuItemClick(item.snippet.resourceId.videoId);
+              }}
+            >
+              <Checkbox className="menu-checkbox"></Checkbox>
+              {item.snippet.title}
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 400 }}>
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <div
             className="site-layout-background"
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              minHeight: 280,
-            }}
+            style={{ padding: 24, textAlign: "center", minHeight: 800 }}
           >
-            {returnIframMarkup(state.currentVideo || state.firstVideo)}
-          </Content>
-        </Layout>
+            {returnIframMarkup()}
+          </div>
+        </Content>
       </Layout>
-    </div>
+    </Layout>
   );
 };
 
