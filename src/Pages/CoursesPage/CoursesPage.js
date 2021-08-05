102a58d9fe82e861e4d16180873d1edf69ad316c
Ritesh <moriarity5555@gmail.com>
2021-08-05 23:19:11 +0530

    Actions added on all Cards

--- src/Pages/CoursesPage/CoursesPage.js
+++ src/Pages/CoursesPage/CoursesPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import VideoPlayer from "../VideoPlayer/VideoPlayer";
 import img1 from "./1.jpg";
 import img2 from "./2.jpg";
 import img3 from "./3.jpg";
 import "./CoursesPage.css";
-import { Card } from 'antd';
+import { Card, Avatar } from 'antd';
+import {CaretRightOutlined,PlusCircleOutlined,YoutubeOutlined} from '@ant-design/icons';
+import 'antd/dist/antd.css';
+
+const { Meta } = Card;
 
 export default function CoursesPage() {
 
@@ -16,37 +19,80 @@ export default function CoursesPage() {
       </section>
 
       <section className="video-section">  
-          <Card className="video">
-              <Link
-                to={{
-                  pathname: "/video-player",
-                  playlistID: "PLWKjhJtqVAbnSe1qUNMG7AbPmjIG54u88",
-                }}
-              >
-                <img src={img1} alt="Freecodecamp HTML+CSS"></img>
-              </Link>
+          <Card 
+              style={{width:300},{padding:0},{margin:20}}
+              cover={
+                <Link
+                    to={{
+                      pathname: "/video-player",
+                      playlistID: "PLWKjhJtqVAbnSe1qUNMG7AbPmjIG54u88",
+                    }}
+                  >
+                    <img src={img1} alt="Freecodecamp HTML+CSS"></img>
+                </Link>
+              }
+              actions={[
+                <CaretRightOutlined key="Play" />,
+                <PlusCircleOutlined key="Enroll" />,
+                <YoutubeOutlined key="Open In Youtube" />,
+              ]}
+          >     
+          <Meta
+              avatar={<Avatar src="https://clipground.com/images/freecodecamp-logo-6.jpg" />}
+              title="HTML+CSS "
+              description="By FreeCodeCamp"
+          />
           </Card>
- 
-          <Card className="video">
-              <Link
-                to={{
-                  pathname: "/video-player",
-                  playlistID: "PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX",
-                }}
-              >
-                <img src={img2} alt="Traverssy JS"></img>
-              </Link>
+          
+          
+          <Card 
+              style={{width:300},{padding:0},{margin:20}}
+              cover={
+                <Link
+                    to={{
+                      pathname: "/video-player",
+                      playlistID: "PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX",
+                    }}
+                  >
+                    <img src={img2} alt="Traverssy JS"></img>
+                </Link>
+              }
+              actions={[
+                <CaretRightOutlined key="Play" />,
+                <PlusCircleOutlined key="Enroll" />,
+                <YoutubeOutlined key="Open In Youtube" />,
+              ]}
+          >     
+          <Meta
+              avatar={<Avatar src="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj" />}
+              title="Javascript"
+              description="By Traversy Media"
+          />
           </Card>
 
-          <Card className="video">
-            <Link
-              to={{
-                pathname: "/video-player",
-                playlistID: "PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d",
-              }}
-            >
-              <img src={img3} alt="React JS"></img>
-            </Link>
+<Card 
+              style={{width:300},{padding:0},{margin:20}}
+              cover={
+                <Link
+                    to={{
+                      pathname: "/video-player",
+                      playlistID: "PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d",
+                    }}
+                  >
+                    <img src={img3} alt="ReactJS"></img>
+                </Link>
+              }
+              actions={[
+                <CaretRightOutlined key="Play" />,
+                <PlusCircleOutlined key="Enroll" />,
+                <YoutubeOutlined key="Open In Youtube" />,
+              ]}
+          >     
+          <Meta
+              avatar={<Avatar src="https://yt3.ggpht.com/ytc/AKedOLT3v89U-2iVX-78hqPk1-lBIduTcljrKLIH9YJg1A=s176-c-k-c0x00ffffff-no-rj" />}
+              title="React JS"
+              description=""
+          />
           </Card>
       </section>
   </div>
