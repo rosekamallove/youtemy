88ef946add940219908c0dc8c7995ecabfd1ac5e
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-05 13:23:42 +0530

    dashboard ui done

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,17 +1,88 @@
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, Progress, Space } from "antd";
 import React from "react";
-import { Link } from "react-router-dom";
+import "./Dashboard.css";
+
+const { Meta } = Card;
 
 export default function Dashboard() {
   return (
-    <div>
-      <Link
-        to={{
-          pathname: "/video-player",
-          playlistID: "PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB",
-        }}
-      >
-        click
-      </Link>
+    <div className="wrapper">
+      <Space direction="horizontal" align="center" width="80%" size={400}>
+        <div>
+          <h2 className="card-heading">Active Course</h2>
+          <Card
+            style={{ width: 300, margin: 0 }}
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="Card title"
+              description="This is the description"
+            />
+          </Card>
+        </div>
+        <div>
+          <h2 className="card-heading">Progress</h2>
+          <Card
+            style={{ width: 300 }}
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <div className="progress-circle-n">
+              <Progress type="circle" percent={69} width={170}></Progress>
+            </div>
+            <Meta
+              title="Current Course Progress"
+              description="This is the description"
+            />
+          </Card>
+        </div>
+        <div>
+          <h2 className="card-heading">Enroll New</h2>
+          <Card
+            style={{ width: 300, margin: 0 }}
+            cover={
+              <img
+                alt="example"
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d109ce23061557.563311582423a.jpg"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="Card title"
+              description="This is the description"
+            />
+          </Card>
+        </div>
+      </Space>
     </div>
   );
 }
