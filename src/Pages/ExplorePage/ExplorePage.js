32fcbb0e5ba944838a05211cbda050feb47e709c
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-06 21:09:34 +0530

    formatted all files and fixed playlistID Undefined

--- src/Pages/ExplorePage/ExplorePage.js
+++ src/Pages/ExplorePage/ExplorePage.js
@@ -1,43 +1,46 @@
 import "./ExplorePage.css";
-import Footer from '../../Components/Footer/Footer';
-import React from 'react';
-import SearchBar from './SearchBar';
-import instance from '../../apis/youtube';
-import PlaylistsList from './PlaylistsList';
-import {Card } from 'antd';
+import Footer from "../../Components/Footer/Footer";
+import React from "react";
+import SearchBar from "./SearchBar";
+import instance from "../../apis/youtube";
+import PlaylistsList from "./PlaylistsList";
+import { Card } from "antd";
 
 class ExplorePage extends React.Component {
-    state = {
-        playlists: [],
-    }
-    handleSubmit = async (termFromSearchBar) => {
-        const response = await instance.get('/search', {
-            params: {
-                q: termFromSearchBar
-            }
-        })
+  state = {
+    playlists: [],
+  };
+  handleSubmit = async (termFromSearchBar) => {
+    const response = await instance.get("/search", {
+      params: {
+        q: termFromSearchBar,
+      },
+    });
 
-        this.setState({
-            playlists: response.data.items
-        })
-        console.log("this is resp",response);
-    };
+    this.setState({
+      playlists: response.data.items,
+    });
+    console.log("this is resp", response);
+  };
 
-    render() {
-        return (
-            <div className='ui container'>
-              <div className='mainbody'>
-                      <Card className="searchbar" style={{width:500},{padding:0},{margin:20}}>
-                          <SearchBar handleFormSubmit={this.handleSubmit}/>
-                      </Card>
-                      < Card className="allplaylists">
-                              <PlaylistsList playlists={this.state.playlists}/>
-                        </Card>
-              </div>
-              <Footer/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <div className="mainbody">
+          <Card
+            className="searchbar"
+            style={({ width: 500 }, { padding: 0 }, { margin: 20 })}
+          >
+            <SearchBar handleFormSubmit={this.handleSubmit} />
+          </Card>
+          <Card className="allplaylists">
+            <PlaylistsList playlists={this.state.playlists} />
+          </Card>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default ExplorePage;
--- src/Pages/ExplorePage/PlaylistItem.js
+++ src/Pages/ExplorePage/PlaylistItem.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import './ExplorePage.css';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ExplorePage.css";
 
-const PlaylistItem = ({key, playlist}) => {
-    return (
-        <div>
-            <Link
-            to={{
-                pathname: "/video-player",
-                playlistID: key,
-            }}>
-                <img className='ui image' src={playlist.snippet.thumbnails.default.url} alt={playlist.snippet.description}/>
-                <h4 className='header '>{playlist.snippet.title}</h4>
-            </Link>
-            <div className="channel-name">{playlist.snippet.channelTitle}</div>
-            <br/><br/>
-        </div>
-    )
+const PlaylistItem = ({ key, playlistID, playlist }) => {
+  return (
+    <div>
+      <Link
+        to={{
+          pathname: "/video-player",
+          playlistID,
+        }}
+      >
+        <img
+          className="ui image"
+          src={playlist.snippet.thumbnails.default.url}
+          alt={playlist.snippet.description}
+        />
+        <h4 className="header ">{playlist.snippet.title}</h4>
+      </Link>
+      <div className="channel-name">{playlist.snippet.channelTitle}</div>
+      <br />
+      <br />
+    </div>
+  );
 };
 export default PlaylistItem;
