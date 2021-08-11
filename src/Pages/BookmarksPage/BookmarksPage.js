33e292932d08abd5be07e033f17bbace2e87bfd3
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-08-11 23:54:30 +0530

    fix: add course from bookmarks page

--- src/Pages/BookmarksPage/BookmarksPage.js
+++ src/Pages/BookmarksPage/BookmarksPage.js
@@ -55,7 +55,7 @@ export default function BookmarksPage() {
                 <PlusCircleOutlined
                   key="Enroll"
                   onClick={() => {
-                    handleAddCourse(playlist, uid);
+                    handleAddCourse(playlist.playlistID, uid);
                   }}
                 />,
                 <DeleteOutlined
